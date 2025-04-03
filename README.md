fc21c946471f39b970a1269c1b2a7ce6b920ee67
pâm <107277080+pamsales@users.noreply.github.com>
2025-04-03 21:46:49 +0000

    adiciona  e

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Treinamento | Implementação do https://www.tabnews.com.br para o curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
-function Home(){
-    return <h1>amo tu tatu</h1>
+function Home() {
+  return <h1>amo tu tatu</h1>;
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2(){
+    console.log('teste2')
 }
 
 export default Home;
